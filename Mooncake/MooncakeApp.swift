6e12d9586a474aeb3e31ee7c74834d7a8d364837
kvalrani <kvalrani.19@gmail.com>
2023-02-06 23:22:44 -0500

    Initial Commit

---
+++ Mooncake/MooncakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MooncakeApp.swift
+//  Mooncake
+//
+//  Created by Kunal Valrani on 2/6/23.
+//
+
+import SwiftUI
+
+@main
+struct MooncakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
