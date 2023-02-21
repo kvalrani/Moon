3e67d6e4832d04b2170bd04ead1818615f241e39
alexruhl11 <aruhl@seas.upenn.edu>
2023-02-20 20:57:17 -0500

    updated cal

--- Mooncake/BetterCalendar.swift
+++ Mooncake/BetterCalendar.swift
@@ -6,15 +6,27 @@
 //
 
 import SwiftUI
+import FSCalendar
 
-struct BetterCalendar: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+class BetterCalendar: UIViewController, FSCalendarDelegate {
+    var calendar = FSCalendar()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        calendar.delegate = self
     }
-}
-
-struct BetterCalendar_Previews: PreviewProvider {
-    static var previews: some View {
-        BetterCalendar()
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        calendar.frame = CGRect(x: 0, y: 100, width: view.frame.size.width, height: view.frame.size.width)
+        view.addSubview(calendar)
     }
+    
+    func calendar (_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "EEEE MM-dd-YYYY"
+        let string = formatter.string(from: date)
+        print("\(string)")
+    }
+    
 }
