9c146c86427ddad99671a95f7c73e7faf556926c
alexruhl11 <aruhl@seas.upenn.edu>
2023-02-20 20:11:57 -0500

    updated

---
+++ Mooncake/BetterCalendar.swift
@@ -0,0 +1,20 @@
+//
+//  BetterCalendar.swift
+//  Mooncake
+//
+//  Created by Alex Ruhl on 2/20/23.
+//
+
+import SwiftUI
+
+struct BetterCalendar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BetterCalendar_Previews: PreviewProvider {
+    static var previews: some View {
+        BetterCalendar()
+    }
+}
