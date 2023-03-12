2f29c00c5c9746e8b48320bff8b7c34aaa462a5a
kvalrani <kvalrani.19@gmail.com>
2023-03-12 16:51:23 -0400

    Merge remote-tracking branch 'refs/remotes/origin/main'

---
+++ Mooncake/Feature/Feedback/Views/FeedbackView.swift
@@ -0,0 +1,45 @@
+//
+//  HomeView.swift
+//  Mooncake
+//
+//  Created by Pranav Aurora on 2/20/23.
+//
+import SwiftUI
+
+struct FeedbackView: View {
+    
+    @EnvironmentObject var service: SessionServiceImpl
+    
+    var body: some View {
+            VStack(alignment: .leading
+                   ) {
+                Spacer()
+                
+                VStack(alignment: .leading,
+                       spacing: 16) {
+                    Text("First Name: \(service.userDetails?.firstName ?? "N/A")").font(Font.custom("Inter-SemiBold", size: 24))
+                    Text("Last Name: \(service.userDetails?.lastName ?? "N/A")").font(Font.custom("Inter-SemiBold", size: 24))
+                    Text("Occupation: \(service.userDetails?.occupation ?? "N/A")").font(Font.custom("Inter-SemiBold", size: 24))
+                    Text("Gender: \(service.userDetails?.gender ?? "N/A")").font(Font.custom("Inter-SemiBold", size: 24))
+                }
+                Spacer()
+                    
+                    ButtonView(title: "Logout") {
+                        service.logout()
+                    }
+                Spacer()
+                
+            }
+            .padding(.horizontal, 16)
+            .navigationTitle("Give Us Feedback!")
+    }
+}
+
+struct FeedbackView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            FeedbackView()
+                .environmentObject(SessionServiceImpl())
+        }
+    }
+}
