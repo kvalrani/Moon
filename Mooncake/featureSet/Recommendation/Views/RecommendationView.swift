755fc5c72e5758e1aac1192fe2974d8c86c6c1f4
kvalrani <kvalrani.19@gmail.com>
2023-03-28 00:41:45 -0400

    wah gi wah

--- Mooncake/featureSet/Recommendation/Views/RecommendationView.swift
+++ Mooncake/featureSet/Recommendation/Views/RecommendationView.swift
@@ -16,6 +16,8 @@ struct RecommendationView: View {
       let price: String
       let time: String
       let allergy: String
+    
+    
 
       init(health: String, cuisine: String, price: String, time: String, allergy: String) {
           self.health = health
@@ -24,31 +26,267 @@ struct RecommendationView: View {
           self.time = time
           self.allergy = allergy
       }
+    var nameM: String = ""
+    var cuisineM: String = ""
+    var healthM: String = ""
+          var priceM: String = ""
+          var timeM: Int = 0
     
     let data = FoodData.foodData
     
-    func findMatchedRestaurant() -> String? {
-        guard let unwrappedData = data else { return nil }
-        for entry in unwrappedData.values {
-            guard let entryDict = entry as? [String: Any],
-                  let name = entryDict["Name"] as? String,
-                  let entryCuisine = entryDict["Cuisine"] as? String,
-                  let entryHealth = entryDict["Health"] as? Int,
-                  let entryPrice = entryDict["Price"] as? Int,
-                  let entryTime = entryDict["Time"] as? Int
-                  else { continue }
-
-            //Kunal and Kush to wrap this up ....
-            if entryCuisine == cuisine{
-                   print("Match found! Name: \(name), Cuisine: \(cuisine), ")
+    func findMatchedRestaurant() -> [Any] {
+            guard let unwrappedData = data else { return [] }
+            for (key, entry) in unwrappedData.enumerated() {
+                guard let entryDict = entry.value as? [String: Any],
+                      let name = entryDict["Name"] as? String,
+                      let entryCuisine = entryDict["Cuisine"] as? String,
+                      let entryHealth = entryDict["Health"] as? Int,
+                      let entryPrice = entryDict["Price"] as? Int,
+                      let entryTime = entryDict["Time"] as? Int
+                      else { continue }
+            
+            var modPrice:Int = 0
+            if price == "$" {
+                modPrice = 1
             }
+            if price == "$$" {
+                modPrice = 2
+            }
+            if price == "$$$" {
+                modPrice = 3
+            }
+            if price == "$$$$" {
+                modPrice = 4
+            }
+            if price == "Any" {
+                modPrice = 0
+            }
+            
+            
+            var modTime:Int = 0
+            if time == "Any" {
+                modTime = 0
+            }
+            if time == "<15 minutes" {
+                modTime = 15
+            }
+            if time == "<30 minutes" {
+                modTime = 30
+            }
+            if time == "<60 minutes" {
+                modTime = 60
+            }
+            if time == "60+ minutes" {
+                modTime = 500
+            }
+            
+            var modHealth:Int = 0
+            if health == "Junk Food" {
+                modHealth = 3
+            }
+            if health == "Healthy" {
+                modHealth = 1
+            }
+            if health == "Any" {
+                modHealth = 0
+            }
+                
+                var modEntryHealth:String = ""
+                if entryHealth == 3 {
+                    modEntryHealth = "Junk Food"
+                }
+                else if entryHealth == 2 {
+                    modEntryHealth = "Medium"
+                }
+                else if entryHealth == 1 {
+                    modEntryHealth = "Healthy"
+                }
+                
+                var modEntryPrice:String = ""
+                if entryPrice == 1 {
+                    modEntryPrice = "$"
+                }
+                else if entryPrice == 2 {
+                    modEntryPrice = "$$"
+                }
+                else if entryPrice == 3 {
+                    modEntryPrice = "$$$"
+                }
+                else if entryPrice == 4 {
+                    modEntryPrice = "$$$$"
+                }
+     
+        
+            
+            if (cuisine == "Any") {
+                
+                if(modPrice > 0) {
+                    if(modHealth > 0) {
+                        if(modTime > 0) {
+                            if ((entryPrice == modPrice) && (entryHealth == modHealth) && (entryTime < modTime)) {
+                                   print("Match found! Name: \(name), Cuisine: \(cuisine), Price: \(price), Time: \(time) ")
+                                var myArray: [Any] = [name, entryCuisine, modEntryHealth, modEntryPrice, entryTime]
+                                return myArray
+                            }
+                            
+                        }
+                        else {
+                            if ((entryPrice == modPrice) && (entryHealth == modHealth)) {
+                                   print("Match found! Name: \(name), Cuisine: \(cuisine), Price: \(price), Time: \(time) ")
+                                var myArray: [Any] = [name, entryCuisine, modEntryHealth, modEntryPrice, entryTime]
+                                return myArray
+                            }
+                        }
+                    }
+                    else {
+                        if(modTime > 0) {
+                            if ((entryPrice == modPrice) && (entryTime < modTime)) {
+                                   print("Match found! Name: \(name), Cuisine: \(cuisine), Price: \(price), Time: \(time) ")
+                                var myArray: [Any] = [name, entryCuisine, modEntryHealth, modEntryPrice, entryTime]
+                                return myArray
+                            }
+                            
+                        }
+                        else {
+                            if ((entryPrice == modPrice)) {
+                                   print("Match found! Name: \(name), Cuisine: \(cuisine), Price: \(price), Time: \(time) ")
+                                var myArray: [Any] = [name, entryCuisine, modEntryHealth, modEntryPrice, entryTime]
+                                return myArray
+                            }
+                        }
+                    }
+                    
+                }
+                else {
+                    if(modHealth > 0) {
+                        if(modTime > 0) {
+                            if ((entryHealth == modHealth) && (entryTime < modTime)) {
+                                print("Match found! Name: \(name), Cuisine: \(cuisine), Price: \(price), Time: \(time) ")
+                                var myArray: [Any] = [name, entryCuisine, modEntryHealth, modEntryPrice, entryTime]
+                                return myArray
+                            }
+                            
+                        }
+                        else {
+                            if ((entryHealth == modHealth)) {
+                                print("Match found! Name: \(name), Cuisine: \(cuisine), Price: \(price), Time: \(time) ")
+                                var myArray: [Any] = [name, entryCuisine, modEntryHealth, modEntryPrice, entryTime]
+                                return myArray
+                            }
+                        }
+                    }
+                    else {
+                        if(modTime > 0) {
+                            if ((entryTime < modTime)) {
+                                print("Match found! Name: \(name), Cuisine: \(cuisine), Price: \(price), Time: \(time) ")
+                                var myArray: [Any] = [name, entryCuisine, modEntryHealth, modEntryPrice, entryTime]
+                                return myArray
+                            }
+                            
+                        }
+                        else {
+                            if(modPrice > 0)
+                            {
+                                if ((entryPrice == modPrice)) {
+                                    print("Match found! Name: \(name), Cuisine: \(cuisine), Price: \(price), Time: \(time) ")
+                                    var myArray: [Any] = [name, entryCuisine, modEntryHealth, modEntryPrice, entryTime]
+                                    return myArray
+                                }
+                            }
+                            else {
+                                print("Match found! Name: \(name), Cuisine: \(cuisine), Price: \(price), Time: \(time) ")
+                                var myArray: [Any] = [name, entryCuisine, modEntryHealth, modEntryPrice, entryTime]
+                                return myArray
+                            }
+                        }
+                    }
+                }
+            }
+            else {
+                if(modPrice > 0) {
+                    if(modHealth > 0) {
+                        if(modTime > 0) {
+                            if ((entryPrice == modPrice) && (entryHealth == modHealth) && (entryTime < modTime) && (entryCuisine == cuisine)) {
+                                   print("Match found! Name: \(name), Cuisine: \(cuisine), Price: \(price), Time: \(time) ")
+                                var myArray: [Any] = [name, entryCuisine, modEntryHealth, modEntryPrice, entryTime]
+                                return myArray
+                            }
+                            
+                        }
+                        else {
+                            if ((entryPrice == modPrice) && (entryHealth == modHealth) && (entryCuisine == cuisine)) {
+                                   print("Match found! Name: \(name), Cuisine: \(cuisine), Price: \(price), Time: \(time) ")
+                                var myArray: [Any] = [name, entryCuisine, modEntryHealth, modEntryPrice, entryTime]
+                                return myArray
+                            }
+                        }
+                    }
+                    else {
+                        if(modTime > 0) {
+                            if ((entryPrice == modPrice) && (entryTime < modTime) && (entryCuisine == cuisine)) {
+                                   print("Match found! Name: \(name), Cuisine: \(cuisine), Price: \(price), Time: \(time) ")
+                                var myArray: [Any] = [name, entryCuisine, modEntryHealth, modEntryPrice, entryTime]
+                                return myArray
+                            }
+                            
+                        }
+                        else {
+                            if ((entryPrice == modPrice) && (entryCuisine == cuisine)) {
+                                   print("Match found! Name: \(name), Cuisine: \(cuisine), Price: \(price), Time: \(time) ")
+                                var myArray: [Any] = [name, entryCuisine, modEntryHealth, modEntryPrice, entryTime]
+                                return myArray
+                            }
+                        }
+                    }
+                    
+                }
+                else {
+                    if(modHealth > 0) {
+                        if(modTime > 0) {
+                            if ((entryPrice == modPrice) && (entryHealth == modHealth) && (entryTime < modTime) && (entryCuisine == cuisine)) {
+                                print("Match found! Name: \(name), Cuisine: \(cuisine), Price: \(price), Time: \(time) ")
+                                var myArray: [Any] = [name, cuisine, health, price, entryTime]
+                                return myArray
+                            }
+                            
+                        }
+                        else {
+                            if ((entryHealth == modHealth) && (entryCuisine == cuisine)) {
+                                print("Match found! Name: \(name), Cuisine: \(cuisine), Price: \(price), Time: \(time) ")
+                                var myArray: [Any] = [name, entryCuisine, modEntryHealth, modEntryPrice, entryTime]
+                                return myArray
+                            }
+                        }
+                    }
+                    else {
+                        if(modTime > 0) {
+                            if ((entryTime < modTime) && (entryCuisine == cuisine)) {
+                                print("Match found! Name: \(name), Cuisine: \(cuisine), Price: \(price), Time: \(time) ")
+                                var myArray: [Any] = [name, entryCuisine, modEntryHealth, modEntryPrice, entryTime]
+                                return myArray
+                            }
+                            
+                        }
+                        else {
+                            if ((entryCuisine == cuisine)) {
+                                print("Match found! Name: \(name), Cuisine: \(cuisine), Price: \(price), Time: \(time) ")
+                                var myArray: [Any] = [name, entryCuisine, modEntryHealth, modEntryPrice, entryTime]
+                                return myArray
+                            }
+                        }
+                    }
+                }
+                
+            }
+            
+
         }
-        return nil
+        return []
     }
     
     var body: some View {
         
-        let matchedRestaurant = findMatchedRestaurant()
+        let matchedRestaurant: [Any] = findMatchedRestaurant()
 
         ZStack {
             VStack(alignment: .leading
@@ -59,7 +297,7 @@ struct RecommendationView: View {
                 {
                     Spacer()
                     //Kunal and Kush to change name
-                    Text("Zahav")
+                    Text("\(NSString(string: "\(matchedRestaurant[0])"))")
                         .font(.largeTitle)
                         .fontWeight(.bold)
                     Spacer()
@@ -71,11 +309,11 @@ struct RecommendationView: View {
                        spacing: 16) {
                     
                     //Kunal and Kush to change these
-                    Text("Cuisine: \(cuisine)").font(.system(size: 24)).fontWeight(.bold).multilineTextAlignment(.center).foregroundColor(Color.white)
-                    Text("Time: \(time)").font(.system(size: 24)).fontWeight(.bold).multilineTextAlignment(.center).foregroundColor(Color.white)
-                    Text("Cost: \(price)").font(.system(size: 24)).fontWeight(.bold).multilineTextAlignment(.center).foregroundColor(Color.white)
-                    Text("Health: \(health)").font(.system(size: 24)).fontWeight(.bold).multilineTextAlignment(.center).foregroundColor(Color.white)
-                    Text("Dietry Preferences: \(allergy)").font(.system(size: 24)).fontWeight(.bold).multilineTextAlignment(.center).foregroundColor(Color.white)
+                    Text("Cuisine: \(NSString(string: "\(matchedRestaurant[1])"))").font(.system(size: 24)).fontWeight(.bold).multilineTextAlignment(.center).foregroundColor(Color.white)
+                    Text("Time: \(NSString(string: "\(matchedRestaurant[4])"))").font(.system(size: 24)).fontWeight(.bold).multilineTextAlignment(.center).foregroundColor(Color.white)
+                    Text("Cost: \(NSString(string: "\(matchedRestaurant[3])"))").font(.system(size: 24)).fontWeight(.bold).multilineTextAlignment(.center).foregroundColor(Color.white)
+                    Text("Health: \(NSString(string: "\(matchedRestaurant[2])"))").font(.system(size: 24)).fontWeight(.bold).multilineTextAlignment(.center).foregroundColor(Color.white)
+                    //Text("Dietry Preferences: \(allergy)").font(.system(size: 24)).fontWeight(.bold).multilineTextAlignment(.center).foregroundColor(Color.white)
                 }
                 Spacer()
                 Spacer()
